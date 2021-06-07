6423c2f57f98dc7ffdd2336b2f321228f0196c3a
maceopaul <anpaul@naver.com>
2021-06-07 07:25:35 +0100

    Modified ADXL345 for python3.x

--- i2c/ADXL345/ADXL345.py
+++ i2c/ADXL345/ADXL345.py
@@ -7,7 +7,7 @@ import time
 bus = smbus.SMBus(1)
 
 # IC address
-address = 0x1D
+address = 0x53
 
 # x-axis, y-axis, z-axis adress
 x_adr = 0x32
@@ -16,6 +16,7 @@ z_adr = 0x36
 
 # ADXL345 init
 def init_ADXL345():    
+    print('ADXL345 init function')
     bus.write_byte_data(address, 0x2D, 0x08)
 
 # data measure
@@ -35,6 +36,7 @@ def measure_acc(adr):
 
 
 def main():
+    print(bus)
     init_ADXL345()
 
     while 1:
@@ -42,7 +44,7 @@ def main():
         y_acc = measure_acc(y_adr)
         z_acc = measure_acc(z_adr)
 
-        print 'X = %2.2f' % x_acc, '[g], Y = %2.2f' % y_acc, '[g], Z = %2.2f' % z_acc, '[g]’
+        print ('X = %2.2f' % x_acc, '[g], Y = %2.2f' % y_acc, '[g], Z = %2.2f' % z_acc, '[g]')
 
         time.sleep(1)
         
